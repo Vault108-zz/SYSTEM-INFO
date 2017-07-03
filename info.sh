64f6bbf63ebdef3b96b60e458c75bc9a5c9d568b
Vault108 <vault108@users.noreply.github.com>
2017-07-03 06:15:39 -0400

    V 1.01

--- info.sh
+++ info.sh
@@ -7,17 +7,17 @@ whiptail --msgbox "
 Press Ok to continue
  " 20 78
 trap 'rm -f choice$$' EXIT
-while whiptail --title Vaults_Sys_Info --menu "Make your choice" 16 78 5 \
-        "1)" "Check Kernel" "2)" "Up Time" "3)" "Free Ram" "4)" "CPU"  "5)" "IP Info" "6)" "Speedtest" 2> choice$$
-do      read CHOICE < choice$$ 
+while whiptail    --title Vaults_Sys_Info --menu "Make your choice"   16 78 5 \
+        "1)" "Check Kernel" "2)" "Up Time" "3)" "Free Ram" "4)" "CPU"  "5)" "IP Info" "6)" "Speedtest"  "7)" "Network Info" 2> choice$$
+do      read CHOICE < choice$$
         case $CHOICE in
         ("1)")  whiptail --textbox /dev/stdin 40 80 <<<"$(uname -r)";;
         ("2)")  whiptail --textbox /dev/stdin 40 80 <<<"$(uptime -p)";;
-        ("3)")  whiptail --textbox /dev/stdin 40 100 <<<"$(free -h > mem.txt && cat mem.txt )";;
+        ("3)")  whiptail --textbox /dev/stdin 40 100 <<<"$(free -h > mem.txt && cat mem.txt && rm mem.txt )";;
         ("4)")  whiptail --textbox /dev/stdin 40 80 <<<"$( cat /proc/cpuinfo | grep 'model name' | uniq )";;
         ("5)")  whiptail --textbox /dev/stdin 40 80 <<<"$(wget -nv -O ip.txt ipinfo.io && cat ip.txt && rm ip.txt)";;
         ("6)")  whiptail --textbox /dev/stdin 40 80 <<<"$(wget -nv https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py && python speedtest.py && rm speedtest.py)";;
-  #      ("7)")  whiptail --textbox /dev/stdin 40 80 <<<"$(uname -r)";;
+        ("7)")  whiptail --textbox /dev/stdin 40 80 <<<"$( ifconfig > if.txt && cat if.txt && rm if.txt)";;
   #      ("8)")  whiptail --textbox /dev/stdin 40 80 <<<"$(uname -r)";;
   #      ("9)")  whiptail --textbox /dev/stdin 40 80 <<<"$(uname -r)";;                    
   #      ("10)")  whiptail --textbox /dev/stdin 40 80 <<<"$(uname -r)";;                      
